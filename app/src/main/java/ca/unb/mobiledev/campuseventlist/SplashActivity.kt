face9c5035dc742a3321fa4440491f11a25ee8c4
ADMSI14 <mohnishdev.budhna@unb.ca>
2025-11-03 20:53:20 -0400

    configured gradle build and added splash view

---
+++ app/src/main/java/ca/unb/mobiledev/campuseventlist/SplashActivity.kt
@@ -0,0 +1,26 @@
+package ca.unb.mobiledev.campuseventlist
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private val splashTimeOut: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Navigate to MainActivity after splash timeout
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+    }
+}
+
