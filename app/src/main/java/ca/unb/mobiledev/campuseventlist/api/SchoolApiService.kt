a14388970231d75dc100700162068fa159931912
ADMSI14 <mohnishdev.budhna@unb.ca>
2025-11-05 22:38:04 -0400

    Add events API endpoint to SchoolApiService

--- app/src/main/java/ca/unb/mobiledev/campuseventlist/api/SchoolApiService.kt
+++ app/src/main/java/ca/unb/mobiledev/campuseventlist/api/SchoolApiService.kt
@@ -1,14 +1,20 @@
 package ca.unb.mobiledev.campuseventlist.api
 
+import ca.unb.mobiledev.campuseventlist.models.EventResponse
 import ca.unb.mobiledev.campuseventlist.models.SchoolResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-// API service to fetch schools from the backend
+// API service to fetch schools and events from the backend
 interface SchoolApiService {
     
     // GET request to retrieve list of schools
     @GET("v1/blog/schools/")
     fun getSchools(): Call<SchoolResponse>
+    
+    // GET request to retrieve events for a specific school
+    @GET("v1/blog/events/school/{schoolId}")
+    fun getEventsBySchool(@Path("schoolId") schoolId: String): Call<EventResponse>
 }
 
